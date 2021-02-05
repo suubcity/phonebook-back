75f9d0a26cbdcd84d5daf2c32e6eab67cd2af5a7
suubcity <suubcity@outlook.com>
2021-02-05 13:07:34 -0700

    Part 3b

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
+
+morgan.token('showData', (req, res) => {
+	return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :showData'));
 
 let persons = [
 	{
